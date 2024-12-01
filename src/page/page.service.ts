414456d50201f4b59d5053969b1ceb5221aaa97c
Edward <batalanedward@gmail.com>
2024-12-01 14:44:01 +0200

    update

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service'; // Assuming PrismaService is in src/prisma.service
+import { PrismaService } from 'src/prisma.service';
 import { CreatePageDto, UpdatePageDto } from './dto/page.dto';
 
 @Injectable()
